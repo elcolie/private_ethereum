03a41f76745bea42c729bf2640565aaa70a6a3a2
Sarit Ritwirune <sarit@elcolie.com>
2021-11-15 21:39:06 +0700

    Add adminModel

--- beauty_coin/profiles/admin.py
+++ beauty_coin/profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from profiles.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)

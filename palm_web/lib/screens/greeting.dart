4b7d91fb6a24aacf6218944d794ab694a65d14b3
Sarit Ritwirune <sarit@elcolie.com>
2022-01-15 12:03:50 +0700

    Add token checking and pass login screen.

--- palm_web/lib/screens/greeting.dart
+++ palm_web/lib/screens/greeting.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:palm_web/screens/balance_screen.dart';
 import 'package:palm_web/screens/signup.dart';
 
 import 'login_screen.dart';
 
 class GreetingScreen extends StatelessWidget {
   static const String routeName = '/greeting';
+
   const GreetingScreen({Key? key}) : super(key: key);
+
+  void checkToken(BuildContext context) async {
+    const storage = FlutterSecureStorage();
+    String? value = await storage.read(key: "jwt");
+    if (value == null) {
+      print("token is empty");
+    } else {
+      print("jwt toekn: " + value);
+      Navigator.pushNamed(context, BalanceScreen.routeName);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    checkToken(context);
     return MaterialApp(
       home: Scaffold(
         body: Center(
@@ -21,17 +36,19 @@ class GreetingScreen extends StatelessWidget {
                 },
                 child: Text("Signup"),
               ),
-              SizedBox(width: 20,),
+              SizedBox(
+                width: 20,
+              ),
               TextButton(
-                onPressed: (){
+                onPressed: () {
                   Navigator.pushNamed(context, LoginScreen.routeName);
                 },
                 child: Text("Login"),
               )
             ],
           ),
-        )
-      )
+        ),
+      ),
     );
   }
 }

4f15f0ddeb460e33a2b644089e255d51971593e6
Sarit Ritwirune <sarit@elcolie.com>
2022-01-16 11:04:01 +0700

    .

--- beauty_coin/beauty_coin/settings.py
+++ beauty_coin/beauty_coin/settings.py
@@ -138,7 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
